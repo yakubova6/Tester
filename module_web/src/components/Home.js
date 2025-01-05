31c5f9ac74ad61926bb4246c53da01b218f70b4a
sanie <bitakov73@mail.ru>
2025-01-05 23:10:02 +0300

    Добавлена гиф

--- module_web/src/components/Home.js
+++ module_web/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/Home.css'; 
 import { useNavigate } from 'react-router-dom';
+import welcomeGif from '../assets/images/welcome.gif'; // Импортируйте ваш GIF с правильным путем
 
 const Home = () => {
     const navigate = useNavigate();
@@ -11,8 +12,14 @@ const Home = () => {
 
     return (
         <div className="container">
-            <h1>Добро пожаловать в наше приложение!</h1>
-            <p className="description">Здесь будет краткое описание нашего приложения.</p>
+            <h1>
+                Добро пожаловать!
+                {' '}
+                <img src={welcomeGif} alt="Welcome GIF" className="welcome-gif" />
+            </h1>
+            <p className="description">Приложение предназначено для проведения массовых опросов и тестирования, доступное как через веб-клиент, так и через Telegram-бота.</p>
+            <p className="description">Для того, чтобы войти в него, нажмите кнопку ниже и используйте ваши учетные данные из внешних сервисов для аутентификации.</p>
+            <p className="description">Мы рады видеть вас в нашем приложении!</p>
             <button className="login-button" onClick={handleLogin}>Войти</button>
         </div>
     );
