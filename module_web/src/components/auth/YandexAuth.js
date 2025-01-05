f9e573d4cf08facfc1c503049c0fcf7b36d54f40
sanie <bitakov73@mail.ru>
2025-01-05 22:04:21 +0300

    Добавлены переменные окружения

--- module_web/src/components/auth/YandexAuth.js
+++ module_web/src/components/auth/YandexAuth.js
@@ -1,12 +1,12 @@
-// src/components/YandexAuth.js
 import React from 'react';
 
 const YandexAuth = () => {
-    const clientId = "60712c9fe7104b4aa8ee392ad5a96c02"; 
-    const redirectUri = "http://localhost:5000/auth/yandex/callback"; 
-    const state = Math.random().toString(36).substring(2); // Генерация случайного состояния
+    const clientId = process.env.REACT_APP_YANDEX_CLIENT_ID; 
+    const redirectUri = process.env.REACT_APP_YANDEX_REDIRECT_URI; 
+    const state = `yandex_${Math.random().toString(36).substring(2)}`;
 
     const handleYandexAuth = () => {
+        localStorage.setItem('auth_state', state);
         window.location.href = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}`;
     };
 
