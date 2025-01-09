a8fd4740b404f102b9b417c88c624f3166c06d72
Teru3301 <timurrumiancev@gmail.com>
2025-01-10 00:43:05 +0300

    инициализация базы данных

--- module main/src/main.cpp
+++ module main/src/main.cpp
@@ -3,56 +3,16 @@
 #include <httplib.h>
 #include <string>
 #include <iostream>
-#include <pqxx/pqxx>
 
 #include "handlerRequest.h"
+#include "PostgreSQL.h"
 
 
 int main ()
 {
 
 //      ПОДКЛЮЧЕНИЕ К PostgreSQL
-
-    std::string PasswordPostreSQL;
-
-    setlocale(LC_ALL, "ru");
-    while (true)
-    {
-        std::cout << "PostgreSQL password: ";
-        std::cin >> PasswordPostreSQL;
-
-        try {
-            // Создаем соединение с базой данных
-            pqxx::connection conn("host=127.0.0.1 port=5432 dbname=postgres user=postgres password=" + PasswordPostreSQL);
-
-            if (conn.is_open()) {
-                std::cout << "Connection to the database has been established!" << std::endl;
-
-                // Создаем транзакцию
-                pqxx::work txn(conn);
-
-                // Выполняем SQL-запрос
-                pqxx::result res = txn.exec("SELECT version()");
-
-                // Выводим результат
-                std::cout << "Version PostgreSQL: " << res[0][0] << std::endl;
-
-                // Завершаем транзакцию
-                txn.commit();
-                std::cout << std::endl;
-                break;
-            } else {
-                std::cerr << "Failed to connect to the database." << std::endl;
-            }
-
-            // Закрываем соединение
-            conn.close();
-        } catch (const std::exception &e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-        }
-    }
-    
-
+    PostgresInit();
 
 
 //      ДОБАВЛЕНИЕ ЭНДПОИНТОВ И НАЧАЛО ПРОСЛУШКИ ПОРТА
@@ -94,34 +54,34 @@ int main ()
 // ..............................
 
 //      вопросы                                                      
-    server.Get("/api/db/quest", GetQuestList);                                  //  Посмотреть список вопросов
-    server.Get("/api/db/quest/(\\d+)/(\\d+)", GetQuestInfo);                    //  Посмотреть информацию о вопросе id вопроса и id версии вопроса
-    server.Put("/api/db/quest(\\d+)", SetQuestInfo);                            //  Изменить текст вопроса/ответов (создаётся новая версия)
-    server.Post("/api/db/quest", AddQuest);                                     //  Создать вопрос
-    server.Delete("/api/db/quest(\\d+)", DelQuest);                             //  Удалить вопрос
+    server.Get("/api/db/quest", GetQuestList);                                                  //  Посмотреть список вопросов
+    server.Get("/api/db/quest/(\\d+)/(\\d+)", GetQuestInfo);                                    //  Посмотреть информацию о вопросе id вопроса и id версии вопроса
+    server.Put("/api/db/quest(\\d+)", SetQuestInfo);                                            //  Изменить текст вопроса/ответов (создаётся новая версия)
+    server.Post("/api/db/quest", AddQuest);                                                     //  Создать вопрос
+    server.Delete("/api/db/quest(\\d+)", DelQuest);                                             //  Удалить вопрос
 //  ..............................
 
 //      тесты 
-    server.Delete("/api/db/disciplines/(\\d+)/tests/(\\d+)/quest(\\d+)", DelTestQuest);                 //  Удалить вопрос из теста
-    server.Post("/api/db/disciplines/(\\d+)/tests/(\\d+)/quest(\\d+)", AddTestQuest);                   //  Добавить вопрос в тест
-    server.Put("/api/db/disciplines/(\\d+)/tests/(\\d+)/questSequence", SetTestQuestSequence);          //  Изменить порядок следования вопросов в тесте
-    server.Get("/api/db/disciplines/(\\d+)/tests/(\\d+)/users", GetQuestUsers);                         //  Посмотреть список пользователей прошедших тест
-    server.Get("/api/db/disciplines/(\\d+)/tests/(\\d+)/gread", GetQuestUsers);                  //  Посмотреть оценку пользователя
-    server.Get("/api/db/disciplines/(\\d+)/tests/(\\d+)/answer", GetTestAnswers);                //  Посмотреть ответы пользователя
+    server.Delete("/api/db/disciplines/(\\d+)/tests/(\\d+)/quest(\\d+)", DelTestQuest);         //  Удалить вопрос из теста
+    server.Post("/api/db/disciplines/(\\d+)/tests/(\\d+)/quest(\\d+)", AddTestQuest);           //  Добавить вопрос в тест
+    server.Put("/api/db/disciplines/(\\d+)/tests/(\\d+)/questSequence", SetTestQuestSequence);  //  Изменить порядок следования вопросов в тесте
+    server.Get("/api/db/disciplines/(\\d+)/tests/(\\d+)/users", GetQuestUsers);                 //  Посмотреть список пользователей прошедших тест
+    server.Get("/api/db/disciplines/(\\d+)/tests/(\\d+)/gread", GetQuestUsers);                 //  Посмотреть оценку пользователя
+    server.Get("/api/db/disciplines/(\\d+)/tests/(\\d+)/answer", GetTestAnswers);               //  Посмотреть ответы пользователя
 //  ..............................
 
 //      попытки
-    server.Post("/api/db/disciplines/(\\d+)/test/(\\d+)/attempt", AddAttempt);          //  Создать
-    server.Put("/api/db/disciplines/(\\d+)/test/(\\d+)/attempt", SetAttempt);           //  Изменить
-    server.Post("/api/db/disciplines/(\\d+)/test/(\\d+)/attempt/finish", FinAttempt);   //  Завершить
-    server.Get("/api/db/disciplines/(\\d+)/test/(\\d+)/attempt", GetAttempt);           //  Посмотреть
+    server.Post("/api/db/disciplines/(\\d+)/test/(\\d+)/attempt", AddAttempt);                  //  Создать
+    server.Put("/api/db/disciplines/(\\d+)/test/(\\d+)/attempt", SetAttempt);                   //  Изменить
+    server.Post("/api/db/disciplines/(\\d+)/test/(\\d+)/attempt/finish", FinAttempt);           //  Завершить
+    server.Get("/api/db/disciplines/(\\d+)/test/(\\d+)/attempt", GetAttempt);                   //  Посмотреть
 //  ..............................
 
 //      ответы
-    server.Post("/api/db/disciplines/(\\d+)/test/(\\d+)/quest/(\\d+)", AddAnswer);      //  Создать
-    server.Get("/api/db/disciplines/(\\d+)/test/(\\d+)/quest/(\\d+)", GetAnswer);       //  Посмотреть
-    server.Put("/api/db/disciplines/(\\d+)/test/(\\d+)/quest/(\\d+)", ChangeAnswer);    //  Изменить
-    server.Delete("/api/db/disciplines/(\\d+)/test/(\\d+)/quest/(\\d+)", DelAnswer);    //  Удалить
+    server.Post("/api/db/disciplines/(\\d+)/test/(\\d+)/quest/(\\d+)", AddAnswer);              //  Создать
+    server.Get("/api/db/disciplines/(\\d+)/test/(\\d+)/quest/(\\d+)", GetAnswer);               //  Посмотреть
+    server.Put("/api/db/disciplines/(\\d+)/test/(\\d+)/quest/(\\d+)", ChangeAnswer);            //  Изменить
+    server.Delete("/api/db/disciplines/(\\d+)/test/(\\d+)/quest/(\\d+)", DelAnswer);            //  Удалить
 //  ..............................
 
 
