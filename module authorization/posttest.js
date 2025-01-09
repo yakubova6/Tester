0ddd29bf6702d9980812c4523e94409cfbeae125
mrxcry <lovegirlrussia@bk.ru>
2025-01-09 18:44:14 +0300

    sanie.js

--- module authorization/posttest.js
+++
@@ -1,9 +0,0 @@
-
-
-const data = [
-    { name: "Иван Иванов", age: 25, email: "ivan@example.com" },
-    { name: "Мария Смирнова", age: 30, email: "maria@example.com" },
-    { name: "Алексей Петров", age: 35, email: "alexey@example.com" }
-];
-
-addUser(data);
